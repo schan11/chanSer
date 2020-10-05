4a8814d6fec729857f1089bf83bb858655ea9452
schan11 <71610534+schan11@users.noreply.github.com>
2020-10-05 00:58:19 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "euler.h"
+
+int main(){
+    printf("The sum of all the multiples of 3 or 5 below 1000 is %d\n", problem1());
+    printf("The smallest positive number that is evenly divisible by all of the numbers from 1 to 20 is %d\n", problem5()); 
+    return 0;
+}
